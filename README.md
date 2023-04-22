193fe188ba2aa60d33126377d108d62d9a223ea9
Nigel T. Crowther <ncrowther@uk.ibm.com>
2023-04-22 08:54:17 +0100

    First release

---
+++ README.md
@@ -0,0 +1,45 @@
+#  Excel to DMN Decision Table Generator
+
+## Description
+
+This asset generates DMN decision tables from Excel.  
+Its primary purpose is for large decision tables to be created and maintained in excel and then generated into DMN.  
+
+### Build environment
+
+You will need:
+
+* Java 11+ installed
+* Environment variable JAVA_HOME set accordingly
+* Maven 3.5.2+ installed
+
+## Create Excel Decision Table
+
+The type of condition and action can be specified by appending a colon and then the DMN type. <br>
+Multiple conditions but only a single action column is supported. <br>
+Excel must be saved as CSV into folder `\excel` <br>
+
+Example format here:  [WeatherAdvice.csv](\excel\WeatherAdvice.csv)
+
+
+## Generate DMN
+
+* Open [ExcelToDMNGenerator.java](\src\main\java\com\ibm\generator\ExcelToDMNGenerator.java)
+* Right click and Run Java
+* DMN files are generated in `\generated`
+* Edit DMN files and replace input condition variables with real DMN inputs
+* Move DMN file to `\src\main\resources`
+* To build and run DMN see next section
+
+#### Compile and Run in Local Dev Mode
+
+```sh
+mvn clean compile quarkus:dev
+```
+
+## API
+
+```sh
+http://localhost:8080/q/swagger-ui
+```
+
---
+++ src/main/java/com/ibm/generator/ExcelToDMNGenerator.java
@@ -0,0 +1,340 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ibm.generator;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
+public class ExcelToDMNGenerator {
+
+    enum DmnDataType {
+        ANY("Any"),
+        BOOLEAN("boolean"),
+        CONTEXT("context"),
+        DATE("date"),
+        DATE_TIME("date and time"),
+        DAYS_TIME("days and time duration"),
+        NUMBER("number"),
+        STRING("string"),
+        TIME("time"),
+        YEARS_MONTHS("years and months duration");
+
+        private String dataType;
+
+        DmnDataType(String dataType) {
+            this.dataType = dataType;
+        }
+
+        public String getDataType() {
+            return dataType;
+        }
+
+        public static boolean validate(String dataTypeStr) {
+            for (DmnDataType dt : DmnDataType.values()) {
+                if (dt.dataType.equalsIgnoreCase(dataTypeStr)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static DmnDataType get(String dataTypeStr) {
+            for (DmnDataType dt : DmnDataType.values()) {
+                if (dt.dataType.equalsIgnoreCase(dataTypeStr)) {
+                    return dt;
+                }
+            }
+            return DmnDataType.ANY;
+        }
+    }
+
+    class TableColumn {
+
+        String columnName;
+        DmnDataType dataType;
+        List<String> cells;
+
+        @Override
+        public String toString() {
+            return "TableColumn [cells=" + cells + "]";
+        }
+    }
+
+    private static final String BASE_PATH = ".";
+    private List<TableColumn> tableColumns;
+    private int tableRows;
+
+    public void readCsvFiles() {
+        String csvFolder = BASE_PATH + "/excel";
+        final File folder = new File(csvFolder);
+        final List<File> filenames = listFilesForFolder(folder);
+
+        Iterator<File> iter = filenames.iterator();
+
+        while (iter.hasNext()) {
+            tableColumns = new ArrayList<TableColumn>();
+            tableRows = 0;
+            File file = iter.next();
+            readCsv(file.getAbsolutePath());
+            generateDMN(file.getName());
+        }
+    }
+
+    private List<File> listFilesForFolder(final File folder) {
+
+        List<File> filenames = new LinkedList<File>();
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                if (fileEntry.getName().contains(".csv"))
+                    filenames.add(fileEntry);
+                System.out.println(fileEntry);
+            }
+        }
+        return filenames;
+    }
+
+    private void setColumnNameAndType(String columnName, TableColumn tableCol) {
+
+        String[] colNameAndType = columnName.split(":"); //splits the string based on :    
+
+        columnName = colNameAndType[0];
+        String dataTypeStr = "<Undefined>"; // default
+        if (colNameAndType.length > 1) {
+            System.out.println("Type: " + colNameAndType[1]);
+            dataTypeStr = colNameAndType[1];
+        }
+
+        tableCol.columnName = columnName;
+        if (DmnDataType.validate(dataTypeStr)) {
+            tableCol.dataType = DmnDataType.get(dataTypeStr);
+        } else {
+            tableCol.dataType = DmnDataType.ANY;
+        }
+    }
+
+    private void readCsv(String csvFile) {
+        String line = "";
+        String csvSplitBy = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            int row = 0;
+
+            // Iterate over each row in decision table
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] data = line.split(csvSplitBy);
+
+                int d = 0;
+                // Iterate over each input col
+                while (d < data.length) {
+
+                    if (tableColumns.size() < d + 1) {
+                        TableColumn tableCol = new TableColumn();
+                        tableCol.cells = new ArrayList<String>();
+
+                        String columnName = data[d];
+                        setColumnNameAndType(columnName, tableCol);
+
+                        tableColumns.add(tableCol);
+                    } else {
+                        TableColumn tableCol = tableColumns.get(d);
+
+                        tableCol.cells.add(data[d]);
+                    }
+                    d++;
+                }
+
+                System.out.println(tableColumns);
+
+                row++;
+            }
+
+            tableRows = row - 1;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Rows read from CSV: " + tableRows);
+
+    }
+
+    private void generateDMN(String filename) {
+
+        String dmnModelName = filename.replace(".csv", "");
+        String header = DMN_HEADER.replace("FILENAME", dmnModelName);
+
+        filename = filename.replace("csv", "dmn");
+        String dmnFile = BASE_PATH + "/generated/" + filename;
+
+        try {
+            // create a new DMN table in XML format
+            int i;
+            StringBuilder dmnTable = new StringBuilder(header);
+
+            generateOutputName(dmnTable);
+            generateHeadings(dmnTable);
+
+            for (i = 0; i < tableRows; i++) {
+                int col = 0;
+
+                String ruleId = "_" + UUID.randomUUID().toString();
+                dmnTable.append("<dmn:rule id=\"" + ruleId + "\">\n");
+
+                // Iterate over each input col
+                while (col < tableColumns.size() - 1) {
+                    TableColumn inputCol = tableColumns.get(col);
+
+                    String inputXml = inputCol.cells.get(i);
+
+                    if (inputXml.equals("")) {
+                        inputXml = "-"; // don't care if condition cell is empty
+                    } else if (inputCol.dataType == DmnDataType.STRING) {
+                        inputXml = "\"" + inputXml + "\""; // String values need double quotes (&#34;) in XML
+                    }
+
+                    inputXml = inputXml.replace(">", "&gt;");
+                    inputXml = inputXml.replace("<", "&lt;");
+
+                    String inputUuid = "_" + UUID.randomUUID().toString();
+
+                    dmnTable.append("<dmn:inputEntry id=\"" + inputUuid + "\">\n");
+                    dmnTable.append("<dmn:text>" + inputXml + "</dmn:text>\n");
+                    dmnTable.append("</dmn:inputEntry>\n");
+
+                    col++;
+                }
+
+                // OutputCol is always last column
+                TableColumn outputCol = tableColumns.get(col);
+                String outputXML = outputCol.cells.get(i);
+                String outputUuid = "_" + UUID.randomUUID().toString();
+
+                dmnTable.append("<dmn:outputEntry id=\"" + outputUuid + "\">\n");
+                dmnTable.append("<dmn:text>\"" + outputXML + "\"</dmn:text>\n");
+                dmnTable.append("</dmn:outputEntry>\n");
+
+                dmnTable.append("<dmn:annotationEntry>\n");
+                dmnTable.append("<dmn:text/>\n");
+                dmnTable.append("</dmn:annotationEntry>\n");
+
+                dmnTable.append("</dmn:rule>\n");
+            }
+
+            System.out.println("Rows writen to DMN table: " + i);
+
+            String dmnModel = dmnTable + "\n" + DMN_TAIL;
+
+            FileWriter myWriter = new FileWriter(dmnFile);
+            myWriter.write(dmnModel);
+            myWriter.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void generateHeadings(StringBuilder dmnTable) {
+
+        int col = 0;
+
+        // Iterate over each input col
+        while (col < tableColumns.size() - 1) {
+            TableColumn inputCol = tableColumns.get(col);
+
+            String inputUuid = "_" + UUID.randomUUID().toString();
+            String expressionUuid = "_" + UUID.randomUUID().toString();
+
+            dmnTable.append("<dmn:input id=\"" + inputUuid + "\">\r\n" +
+                    "        <dmn:inputExpression id=\"" + expressionUuid + "\" typeRef=\"" + inputCol.dataType.dataType + "\">\r\n" +
+                    "          <dmn:text>_" + inputCol.columnName + "_</dmn:text>\r\n" +
+                    "        </dmn:inputExpression>\r\n" +
+                    "      </dmn:input>");
+
+            col++;
+        }
+        String outputUuid = "_" + UUID.randomUUID().toString();
+        dmnTable.append("<dmn:output id=\"" + outputUuid + "\"/>\r\n" +
+                "      <dmn:annotation name=\"annotation-1\"/>");
+
+    }
+
+    private void generateOutputName(StringBuilder dmnTable) {
+
+        TableColumn outputCol = tableColumns.get(tableColumns.size() - 1);
+
+        String decisionUuid = "_BBA8FB3E-8E56-41E2-A9D5-80FECE01097C"; //"_" + UUID.randomUUID().toString();
+        String variableUuid = "_" + UUID.randomUUID().toString();
+        String decisionTableUuid = "_7CCC4E09-15AD-4108-A087-287BAACE06B3"; //"_" + UUID.randomUUID().toString();
+        dmnTable.append("  <dmn:decision id=\"" + decisionUuid + "\" name=\"" + outputCol.columnName + "\">\r\n" +
+                "    <dmn:extensionElements/>\r\n" +
+                "    <dmn:variable id=\"" + variableUuid + "\" name=\"" + outputCol.columnName + "\" typeRef=\"" + outputCol.dataType.dataType + "\"/>\r\n" +
+                "    <dmn:decisionTable id=\"" + decisionTableUuid + "\" hitPolicy=\"FIRST\" preferredOrientation=\"Rule-as-Row\">");
+    }
+
+    private static String DMN_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" +
+            "<dmn:definitions xmlns:dmn=\"http://www.omg.org/spec/DMN/20180521/MODEL/\" xmlns=\"https://www.drools.org/kie-dmn/FILENAME\" xmlns:feel=\"http://www.omg.org/spec/DMN/20180521/FEEL/\" xmlns:kie=\"http://www.drools.org/kie/dmn/1.2\" xmlns:dmndi=\"http://www.omg.org/spec/DMN/20180521/DMNDI/\" xmlns:di=\"http://www.omg.org/spec/DMN/20180521/DI/\" xmlns:dc=\"http://www.omg.org/spec/DMN/20180521/DC/\" id=\"_D22C5EC8-4E20-46AD-A8F0-5909A7F4A319\" name=\"FILENAME\" typeLanguage=\"http://www.omg.org/spec/DMN/20180521/FEEL/\" namespace=\"https://www.drools.org/kie-dmn/FILENAME\">\r\n"
+            +
+            "  <dmn:extensionElements/>\r\n\r\n";
+
+    private static final String DMN_TAIL = "    </dmn:decisionTable>\r\n" +
+            "  </dmn:decision>\r\n" +
+            "  <dmndi:DMNDI>\r\n" +
+            "    <dmndi:DMNDiagram id=\"_29F4A98A-07F6-46C3-BB7E-007BB02F7B94\" name=\"DRG\">\r\n" +
+            "      <di:extension>\r\n" +
+            "        <kie:ComponentsWidthsExtension>\r\n" +
+            "          <kie:ComponentWidths dmnElementRef=\"_7CCC4E09-15AD-4108-A087-287BAACE06B3\">\r\n" +
+            "            <kie:width>50</kie:width>\r\n" +
+            "            <kie:width>100</kie:width>\r\n" +
+            "            <kie:width>100</kie:width>\r\n" +
+            "            <kie:width>100</kie:width>\r\n" +
+            "            <kie:width>100</kie:width>\r\n" +
+            "          </kie:ComponentWidths>\r\n" +
+            "        </kie:ComponentsWidthsExtension>\r\n" +
+            "      </di:extension>\r\n" +
+            "      <dmndi:DMNShape id=\"dmnshape-drg-_BBA8FB3E-8E56-41E2-A9D5-80FECE01097C\" dmnElementRef=\"_BBA8FB3E-8E56-41E2-A9D5-80FECE01097C\" isCollapsed=\"false\">\r\n" +
+            "        <dmndi:DMNStyle>\r\n" +
+            "          <dmndi:FillColor red=\"255\" green=\"255\" blue=\"255\"/>\r\n" +
+            "          <dmndi:StrokeColor red=\"0\" green=\"0\" blue=\"0\"/>\r\n" +
+            "          <dmndi:FontColor red=\"0\" green=\"0\" blue=\"0\"/>\r\n" +
+            "        </dmndi:DMNStyle>\r\n" +
+            "        <dc:Bounds x=\"565\" y=\"184\" width=\"100\" height=\"50\"/>\r\n" +
+            "        <dmndi:DMNLabel/>\r\n" +
+            "      </dmndi:DMNShape>\r\n" +
+            "    </dmndi:DMNDiagram>\r\n" +
+            "  </dmndi:DMNDI>\r\n" +
+            "</dmn:definitions>";
+
+    public static void main(String[] args) {
+
+        ExcelToDMNGenerator xmlGenerator = new ExcelToDMNGenerator();
+
+        xmlGenerator.readCsvFiles();
+    }
+
+}
